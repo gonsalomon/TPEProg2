7be03f5e922b6e697a7776c69acb594df96ee50a
gonsalomon <62311853+gonsalomon@users.noreply.github.com>
2021-10-20 16:37:16 -0300

    Add files via upload

---
+++ src/com/company/Contest.java
@@ -0,0 +1,9 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Contest {
+    ArrayList<Song> songList;
+    ArrayList<Coach> coachList;
+    ArrayList<Participant> partList;
+}
