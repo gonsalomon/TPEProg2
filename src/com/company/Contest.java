4d385d0b56c79f74440007f56c07c296f9d98850
gonsalomon <62311853+gonsalomon@users.noreply.github.com>
2021-10-27 11:12:02 -0300

    Delete src/com/company directory

--- src/com/company/Contest.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-import java.util.ArrayList;
-
-public class Contest {
-    ArrayList<Song> songList;
-    ArrayList<Coach> coachList;
-    ArrayList<Participant> partList;
-}
